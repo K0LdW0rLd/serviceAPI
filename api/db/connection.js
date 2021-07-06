1428164fda2929723e92f3774c45548787d0c00f
Kelly M Collins <90kcol@gmail.com>
2021-07-06 21:33:26 +0100

    test

--- api/db/connection.js
+++ api/db/connection.js
@@ -14,27 +14,24 @@ mongoose.Promise = Promise;
 
 // .connect takes two arguments, one is the path to the db
 // second argument where you set your settings for the connection
-mongoose
-  .connect(
-    process.env.MONGODB_URI,
-    {
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-      replset: {
-        socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 },
-      },
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: {
+      socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 },
     },
-    function (err) {
-      if (err) return console.log("Error: ", err);
-      console.log(
-        "MongoDB Connection -- Ready state is:",
-        mongoose.connection.readyState
-      );
-    }
-  )
-  .then((feedback) => console.log("connected to the db"))
-  .catch((error) => console.log("It has broke, here's why ", error));
+  },
+  function (err) {
+    if (err) return console.log("Error: ", err);
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState
+    );
+  }
+);
 module.exports = mongoose;
