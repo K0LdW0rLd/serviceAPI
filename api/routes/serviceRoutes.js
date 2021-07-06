e7b06ec0fafa47809694b89a0fac6cfe7af64ddd
Kelly M Collins <90kcol@gmail.com>
2021-07-06 20:48:32 +0100

    Test

--- api/routes/serviceRoutes.js
+++ api/routes/serviceRoutes.js
@@ -1,6 +1,11 @@
 module.exports = function (app) {
   let service = require("../controllers/serviceController");
   // Routes
+  // add this below app.use("/", routes) to make index.html a static file
+    app.route('/')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/index.html');
+        });
   app.route("/service").get(service.listServices).post(service.createService);
   app
     .route("/service/:serviceId")
--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var express = require("express"),
   Service = require("./api/models/serviceModel"),
   bodyParser = require("body-parser");
 
+const helmet = require("helmet");
+const compression = require("compression");
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/Servicedb");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(compression());
 
 var routes = require("./api/routes/serviceRoutes"); //importing route
 routes(app); //register the route
