5effa404cd38458d323f56a699ce5664867a3efe
Kelly M Collins <90kcol@gmail.com>
2021-07-06 21:11:55 +0100

    Updated server.js and connection.js

--- api/db/connection.js
+++ api/db/connection.js
@@ -4,21 +4,37 @@ const mongoose = require("mongoose");
 // Reset promise
 mongoose.Promise = Promise;
 
-let mongoURI = "";
+// let mongoURI = "";
 
-if (process.env.NODE_ENV === "production") {
-  mongoURI = process.env.DB_URL;
-} else {
-  mongoURI = "mongodb://localhost/service";
-}
+// if (process.env.NODE_ENV === "production") {
+//   mongoURI = process.env.DB_URL;
+// } else {
+//   mongoURI = "mongodb://localhost/service";
+// }
 
 // .connect takes two arguments, one is the path to the db
 // second argument where you set your settings for the connection
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useMongoClient: true,
-  })
+  .connect(
+    process.env.MONGODB_URI,
+    {
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+      replset: {
+        socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 },
+      },
+    },
+    function (err) {
+      if (err) return console.log("Error: ", err);
+      console.log(
+        "MongoDB Connection -- Ready state is:",
+        mongoose.connection.readyState
+      );
+    }
+  )
   .then((feedback) => console.log("connected to the db"))
   .catch((error) => console.log("It has broke, here's why ", error));
 module.exports = mongoose;
